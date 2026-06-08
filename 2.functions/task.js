0ef108b511ffa014a8827547d82bd031af624c9b
Nick5ohm <nickson@MacBook-Pro-NicksonNikita.local>
2026-06-09 01:12:42 +0300

    Первая часть дз

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,25 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  if (!arr.length){
+    return "Nothing";
+  }
+  let min = arr[0];
+  for (let num of arr){
+    if (num < min){
+      min = num;
+    }
+  }
+  let max = arr[0];
+    for (let num of arr){
+    if (num > max){
+      max = num;
+    }
+  }
+  let sum = 0;
+  for (let num of arr){
+    sum += num;
+  }
+  let avg = sum/arr.length;
+  return { min: min, max: max, avg: +avg.toFixed(2)};
 }
 
 function summElementsWorker(...arr) {
